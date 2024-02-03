e8e72e9e11c7fc930c8d48c65eb05bc86d5a2e42
kashimmirza <kashimmirza86.com>
2024-02-03 17:52:04 +0600

    this the medium problem from leetcode to find the maximum consequitive sequence in the integer array, I have done this using two pointer

---
+++ LongestConsequitiveSequence.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+    int[] nums =  { 0,0 };
+    LongestConsecutive(nums);
+
+
+    int LongestConsecutive(int[] nums)
+    {
+        int longconse = 1;
+        Array.Sort(nums);
+    int cntconse = 1;
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+        if ((nums[i + 1] - nums[i]) == 1 )
+        {
+            cntconse++;
+            if (cntconse > longconse)
+            {
+                longconse = cntconse;
+
+            }
+        }
+        else if ((nums[i + 1] - nums[i])>1)
+        {
+
+            cntconse = 1;
+
+
+        }
+
+        }
+    return longconse;
+       
+    }
+
+
