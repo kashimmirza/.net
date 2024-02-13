6a6ec25a4fd29cad0716da62341152bf113c2a23
kashimmirza <kashimmirza86.com>
2024-02-13 17:49:08 +0600

    intersection between array version 2

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+public partial class Program
+{
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        int[] nums1 = { 1, 2, 2, 1 };
+        int[] nums2 = { 2, 2 };
+        int[] result = solution.Intersect(nums1, nums2);
+        Console.WriteLine("Intersection:");
+        foreach (var num in result)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
